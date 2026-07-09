ecc4e847925eaaecf07cc81cd54d8ddeabc75b29
malinha22 <bruga.amalia@escola.pr.gov.br>
2026-07-09 09:22:17 -0300

    .

---
+++ main.js
@@ -0,0 +1,11 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 5;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
